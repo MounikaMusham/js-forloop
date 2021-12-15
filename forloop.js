25b5f7c031d119cad983039745cecc8354f6f619
mounika <mounikamusham12@gmail.com>
2021-12-15 18:17:37 +0530

    forloop

---
+++ forloop.js
@@ -0,0 +1,13 @@
+
+var sum=1;
+var n=10;
+for (let i=1;i<=n;i++){
+    sum *= i;
+}
+console.log(sum);
+
+// program to print 3 table
+var n=3;
+for(let i=1;i<=10;i++){
+    console.log(n+"*"+i+ "=" +i*n);
+}
